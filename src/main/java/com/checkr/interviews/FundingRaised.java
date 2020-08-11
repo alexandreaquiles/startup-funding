9d0150fd1306f3a60adca763a81eb857a73f2008
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-08-11 19:34:35 -0300

    Extract Class: Main

--- src/main/java/com/checkr/interviews/FundingRaised.java
+++ src/main/java/com/checkr/interviews/FundingRaised.java
@@ -101,17 +101,6 @@ public class FundingRaised {
         return csvData;
     }
 
-    public static void main(String[] args) {
-        try {
-            Map<String, String> options = new HashMap<> ();
-            options.put("company_name", "Facebook");
-            options.put("round", "a");
-            System.out.print(FundingRaised.where(options).size());
-        } catch(IOException e) {
-            System.out.print(e.getMessage());
-            System.out.print("error");
-        }
-    }
 }
 
 class NoSuchEntryException extends Exception {}
